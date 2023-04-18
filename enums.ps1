5bc946548ce8364759007850f1a307d37280bfcc
Pete Lenhart <62825319+codenamepetrel@users.noreply.github.com>
2023-04-18 15:15:37 -0500

    Create enums.ps1

---
+++ enums.ps1
@@ -0,0 +1,19 @@
+enum order {
+    asc;
+    desc;
+}
+
+enum status {
+    new;
+    inProgress;
+    truePositive;
+    falsePositive;
+    ignored;
+    closed;
+    reopened;
+}
+
+enum quarantine {
+    contain;
+    lift_containment;
+}
