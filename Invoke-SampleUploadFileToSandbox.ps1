2ef67d872ea2bfc9950a66908889aa6c1568a70c
Freeworld Inc <62825319+codenamepetrel@users.noreply.github.com>
2023-04-26 15:55:08 -0500

    Create Invoke-SampleUploadFileToSandbox.ps1

---
+++ Invoke-SampleUploadFileToSandbox.ps1
@@ -0,0 +1,73 @@
+#Pete Lenhart - 4/13/2023
+#Upload file to sandbox
+
+[CmdletBinding()]
+
+Param
+(
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [string]$hostname,
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [string]$uploadfilename
+)
+BEGIN {
+
+    try {
+        . (".\Classes\falconapihelper.ps1")
+        }
+    catch {
+        Write-Host "Error while loading supporting PowerShell Scripts" 
+        Break
+        }
+
+    #$myAPICreds.GetAPIInfo()
+    $myID = $myAPICreds.apiKey
+    $mySec = $myAPICreds.apiSeecret
+
+    $crowdStrikeSession = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+    $crowdStrikeResponse = Invoke-WebRequest -UseBasicParsing -Uri "https://api.crowdstrike.com/oauth2/token" `
+        -Method "POST" `
+        -WebSession $crowdStrikeSession `
+        -Headers @{
+        "Accept-Encoding" = "gzip, deflate, br"
+        "Accept-Language" = "en-US,en;q=0.5"
+        "accept"          = "application/json"
+    } `
+        -ContentType "application/x-www-form-urlencoded" `
+        -Body "client_id=$myID&client_secret=$mySec"
+
+    #GET THE BEARER TOKEN AND PASS IT TO THE NEXT CALL
+    $tokenRespJSON = ConvertFrom-Json $crowdStrikeResponse.Content
+    $tokingBearer = $tokenRespJSON.token_type
+    $token = $tokenRespJSON.access_token
+    #$token
+}
+PROCESS{
+
+    $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+    $session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+        Invoke-WebRequest -UseBasicParsing -Uri "https://api.crowdstrike.com/samples/entities/samples/v2" `
+        -Method "POST" `
+        -WebSession $session `
+        -Headers @{
+        "Accept-Encoding"="gzip, deflate, br"
+        "Accept-Language"="en-US,en;q=0.5"  
+        "accept"="application/json"
+        "authorization"="Bearer $token"
+ 
+} `
+    -ContentType "multipart/form-data; boundary=----WebKitFormBoundaryUTX8mQoeFJlrnQCk" `
+    -Body ([System.Text.Encoding]::UTF8.GetBytes("------WebKitFormBoundaryUTX8mQoeFJlrnQCk$([char]13)$([char]10) `
+        Content-Disposition: form-data; name=`"sample`"; filename=`"$uploadfilename`"$([char]13)$([char]10) `
+        Content-Type: application/x-msdownload$([char]13)$([char]10)$([char]13)$([char]10)$([char]13)$([char]10)------WebKitFormBoundaryUTX8mQoeFJlrnQCk$([char]13)$([char]10) `
+        Content-Disposition: form-data; name=`"file_name`"$([char]13)$([char]10)$([char]13)$([char]10)$uploadfilename$([char]13)$([char]10)------WebKitFormBoundaryUTX8mQoeFJlrnQCk$([char]13)$([char]10) `
+        Content-Disposition: form-data; name=`"comment`"$([char]13)$([char]10)$([char]13)$([char]10)Download file$([char]13)$([char]10)------WebKitFormBoundaryUTX8mQoeFJlrnQCk$([char]13)$([char]10) `
+        Content-Disposition: form-data; name=`"is_confidential`"$([char]13)$([char]10)$([char]13)$([char]10)true$([char]13)$([char]10)------WebKitFormBoundaryUTX8mQoeFJlrnQCk--$([char]13)$([char]10)"))
+}
+END{
+
+    Write-Output "The dishes are done man."
+    
+}
